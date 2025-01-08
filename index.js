e77cba01a394e6b675041894c08b28a102366779
bajracae <aeijanbajracharya@gmail.com>
2025-01-08 00:52:01 -0800

    added basic UI with buttons

--- index.js
+++ index.js
@@ -2,68 +2,61 @@ humanScore = 0;
 computerScore = 0;
 
 function getComputerChoice() {
-  max = 3;
-  randomNumber = Math.floor(Math.random() * max);
-  if (randomNumber == 0) {
-    return "rock";
-  } else if (randomNumber == 1) {
-    return "paper";
-  } else {
-    return "scissors";
-  }
-}
-
-function getHumanChoice() {
-  while (true) {
-    humanChoice = prompt("rock, paper, or scissors? your choice: ");
-    humanChoice = humanChoice.toLowerCase();
-    if (
-      humanChoice == "rock" ||
-      humanChoice == "paper" ||
-      humanChoice == "scissors"
-    ) {
-      return humanChoice;
-    }
-  }
+	max = 3;
+	randomNumber = Math.floor(Math.random() * max);
+	if (randomNumber == 0) {
+		return "rock";
+	} else if (randomNumber == 1) {
+		return "paper";
+	} else {
+		return "scissors";
+	}
 }
 
 function playRound(humanChoice, computerChoice) {
-  message = "";
-  if (humanChoice == computerChoice) {
-    message = "You both tied! Try again.";
-  } else if (humanChoice == "rock" && computerChoice == "scissors") {
-    humanScore += 1;
-    message = "You win! rock beats scissors.";
-  } else if (humanChoice == "paper" && computerChoice == "rock") {
-    humanScore += 1;
-    message = "You win! paper beats rock.";
-  } else if (humanChoice == "scissors" && computerChoice == "paper") {
-    humanScore += 1;
-    message = "You win! scissors beats paper.";
-  } else {
-    computerScore += 1;
-    message = `You lose! ${computerChoice} beats ${humanChoice}.`;
-  }
-  console.log(
-    message + "\n" + `Human: ${humanScore} | Computer: ${computerScore}`
-  );
+	message = "";
+	if (humanChoice == computerChoice) {
+		message = "You both tied! Try again.";
+	} else if (humanChoice == "rock" && computerChoice == "scissors") {
+		humanScore += 1;
+		message = "You win! rock beats scissors.";
+	} else if (humanChoice == "paper" && computerChoice == "rock") {
+		humanScore += 1;
+		message = "You win! paper beats rock.";
+	} else if (humanChoice == "scissors" && computerChoice == "paper") {
+		humanScore += 1;
+		message = "You win! scissors beats paper.";
+	} else {
+		computerScore += 1;
+		message = `You lose! ${computerChoice} beats ${humanChoice}.`;
+	}
+
+	result_message =
+		message + "\n" + `Human: ${humanScore} | Computer: ${computerScore}`;
+
+	if (humanScore == 5) {
+		result_message = `You win the game!`;
+	}
+	if (computerScore == 5) {
+		result_message = `You lost the game!`;
+	}
+
+	updateResult(result_message);
 }
 
-function playGame() {
-  while (true) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-
-    playRound(humanSelection, computerSelection);
-    if (humanScore == 5) {
-      console.log(`You win the game!`);
-      return;
-    }
-    if (computerScore == 5) {
-      console.log(`You lost the game!`);
-      return;
-    }
-  }
+function updateResult(message) {
+	const result = document.querySelector(".result");
+	result.textContent = message;
 }
 
-playGame();
+function runGame() {
+	const buttons = document.querySelectorAll("button");
+
+	buttons.forEach((button) => {
+		button.addEventListener("click", () => {
+			playRound(button.textContent, getComputerChoice());
+		});
+	});
+}
+
+runGame();
